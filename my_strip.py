409eabf5c2e9a6db35843d2bca8fbec3725087a8
Mario Molina <mm2535@cornell.edu>
2015-07-22 10:25:16 -0400

    strip a json file

--- my_strip.py
+++ my_strip.py
@@ -5,20 +5,27 @@ data_1 = []
 
 def m1(myfile):
     """
-    Strips each line in the json file and append it into the list data_1
+    strips each line in the json file and append it into the list data_1.
+    
+    returns the type of the first list's element
     """
     data_json = open(myfile)
     
     for line in data_json:
         data_1.append(line.strip())
     
-    return type(data_1[0])
-
+    return data_1[0:5]
 
 data_2 = []
 def m2(mylist):
+    """
+    take each element of mylist and load it as json.
+    returns a list of json files.
+    """
     for item in mylist:
         data_2.append(json.loads(item))
+    
+    return data_2[0:10]
 
 #def subset():
 #    for item in data_3:
