bbe41e40cc9ae0cdfca8b89883eb731e1dc07c45
Mario Molina <mm2535@cornell.edu>
2015-07-27 22:48:22 -0400

    make an edge list

--- edge_list.py
+++ edge_list.py
@@ -14,5 +14,26 @@ for item in f2:
 f2.close()
 
 
+def add_id(mylist):
+    """
+    this creates an id for each user_id in the list user
+    it returns a list of lists with an id (int) and user_id. 
+    """
+    
+    ids = []
+    
+    for i in range(len(mylist)):
+        n0 = mylist[i].find(':')
+        n1 = mylist[i].find(';')
+        part = mylist[i][n0+2:n1-1]
+        
+        ids.append([i, part])
+        
+        
 def edgelist():
-    pass
+    """
+    """
+    
+    
+        
+    
